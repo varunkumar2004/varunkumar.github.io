639d345ff7efe0a8b0b769c19748dfc1ee98be98
Varun Kumar <varunkumar2004.vk@gmail.com>
2024-07-28 17:18:38 +0530

    responsive changes

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let header_links = document.querySelectorAll('#nav_links')
-let span_contact = document.querySelectorAll('.contact_span')
+let contact_span = document.querySelectorAll('.contact_span')
 let contact_section = document.querySelector('.contact_section')
+let close_contact = document.querySelector('.close_contact')
 
 console.log(contact_section)
+
+close_contact.addEventListener('click', () => {
+    if (contact_section.classList.contains('active')) {
+        contact_section.classList.remove('active')
+    }
+})
 
 header_links.forEach(
     link => {
@@ -10,20 +17,22 @@ header_links.forEach(
             'click', () => {
                 let target = link.getAttribute('--nav_link')
                 let target_element = document.getElementsByClassName(target)[0]
-                target_element.scrollIntoView({ behavior: 'smooth' })
+                try {
+                    target_element.scrollIntoView({ behavior: 'smooth' })
+                } catch(e) {
+                    console.log(e)
+                }
             }
         )
     }
 )
 
-span_contact.forEach(
+contact_span.forEach(
     section => {
         section.addEventListener(
             'click', () => {
                 if (!contact_section.classList.contains('active')) {
                     contact_section.classList.add('active')
-                } else {
-                    contact_section.classList.remove('active')
                 }
             }
         )
